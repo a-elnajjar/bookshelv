6d7b3a28aa0adc7c94a33be56aa5cee149c7587d
Abdalla.Mohamed <abdalla.mohamed@mhs.com>
2021-02-19 12:35:27 -0500

    Initial Commit

---
+++ bookshelv/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bookshelv
+//
+//  Created by Abdalla Elnajjar on 2021-02-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bookshelv/bookshelvApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookshelvApp.swift
+//  bookshelv
+//
+//  Created by Abdalla Elnajjar on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct bookshelvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
