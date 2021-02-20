99d26c897cc58ebe73b91138c0609bce1f80ab87
Abdalla.Mohamed <abdalla.mohamed@mhs.com>
2021-02-19 22:32:20 -0500

    init commit

--- bookshelv/ContentView.swift
+++ bookshelv/ContentView.swift
@@ -6,11 +6,30 @@
 //
 
 import SwiftUI
+import PDFKit
 
 struct ContentView: View {
+    
+    @State private var viewLocalPDF = false
+    let fileUrl = Bundle.main.url(forResource: "swift-style-second-edition_P1.0", withExtension:"pdf")!
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        
+        
+        NavigationView {
+                ZStack {
+                    VStack {
+                        NavigationLink(destination: PDFKitView(url: self.fileUrl), isActive: $viewLocalPDF) {
+                            Button("view book"){
+                                self.viewLocalPDF = true
+                            }
+                            .padding(.bottom, 20)
+                        }
+                    }
+                }
+                .navigationBarTitle("Bookshelv ", displayMode: .inline)
+            }
+        
     }
 }
 
---
+++ bookshelv/PDFKitView.swift
@@ -0,0 +1,38 @@
+//
+//  PDFKitView.swift
+//  bookshelv
+//
+//  Created by Abdalla Elnajjar on 2021-02-19.
+//
+
+import PDFKit
+import SwiftUI
+
+struct PDFKitView: View {
+    var url: URL
+    //var pdfName:String
+    var body: some View {
+        PDFKitRepresentedView(url)
+    }
+}
+
+struct PDFKitRepresentedView: UIViewRepresentable {
+    let url: URL
+    init(_ url: URL) {
+        self.url = url
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<PDFKitRepresentedView>) -> PDFKitRepresentedView.UIViewType {
+        let pdfView = PDFView()
+        pdfView.document = PDFDocument(url: self.url)
+        pdfView.autoScales = true
+        pdfView.usePageViewController(true)
+        pdfView.displayDirection = .horizontal
+        pdfView.displaysPageBreaks = true
+        return pdfView
+    }
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PDFKitRepresentedView>) {
+        // Update the view.
+    }
+}
